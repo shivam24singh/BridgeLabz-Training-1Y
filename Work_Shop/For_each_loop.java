a7af86bad1794ebefad6bbbc177c3ef2506f3f03
shivam24singh <shivamthakur1980as@gmail.com>
2026-03-22 19:30:24 +0530

    :Added:

--- Work_Shop/For_each_loop.java
+++
@@ -1,8 +0,0 @@
-public class For_each_loop {
-    public static void main(String[] args) {
-        int[] array={10,20,30,40,50};
-        for(int num:array){
-           System.err.println(num); 
-        }
-    }
-}
--- Work_Shop/While_basic.java
+++
@@ -1,9 +0,0 @@
-public class while_basic{
-  public static void main(string[]args[]args)
-      int i=1;
-	  while(i<=20){
-	  system.out.println(i);
-	  i+++;
-	  }
-	  }
-	  }
